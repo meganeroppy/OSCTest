255547aac13d770bf600ee4e601c09fefcb769ed
meganeroppy <roppy@hotmail.co.jp>
2017-10-11 17:18:09 +0900

    送信シーンを調整

--- Assets/SendSceneUIController.cs
+++ Assets/SendSceneUIController.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using info.shibuya24.osc;
+using System.Net;
 
-public class SendSceneUIController : MonoBehaviour {
+/// <summary>
+/// 送信テストシーン制御
+/// </summary>
+public class SendSceneUIController : MonoBehaviour 
+{
+	// OSC送信インスタンス
+	OSCSender m_sender;
+	public Slider slider;
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private string clientId = "TestClient";
+	[SerializeField]
+	private int port = 8890;
+	[SerializeField]
+	private string ipAddressStr = "192.168.3.1";
+
+
+	void Start () 
+	{
+		m_sender = new OSCSender ();
+
+		// 送信クライアント名のセット、IPアドレス、ポート番号は適宜調整
+		// このIPアドレスとポート番号は受信側のIPアドレスとポート番号です
+//		m_sender.Init ("TestClient", 8890, IPAddress.Parse ("192.168.3." + Config.ipNum));
+		IPAddress ipAddress;
+		if( IPAddress.TryParse( ipAddressStr, out ipAddress ) )
+		{
+			m_sender.Init (clientId, port, ipAddress);
+		}
+		else
+		{
+			Debug.LogError("無効なIPアドレス -> [ " + ipAddressStr + " ]");
+			return;
+		}
+		var e = new Slider.SliderEvent ();
+		// Sendメソッドで送信
+		e.AddListener (x => m_sender.Send ("/changeValue", x));
+		slider.onValueChanged = e;			
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OnClickButton(int id)
+	{
+		Debug.Log(id);
 	}
 }
