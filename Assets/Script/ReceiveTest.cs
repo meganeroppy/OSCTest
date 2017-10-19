df79535ebd0f83c32aed09819d0532c5ad634661
meganeroppy <roppy@hotmail.co.jp>
2017-10-19 18:31:11 +0900

    遷移中ステートを定義して実装

--- Assets/Script/ReceiveTest.cs
+++ Assets/Script/ReceiveTest.cs
@@ -10,49 +10,57 @@ using UnityOSC;
 public class ReceiveTest : MonoBehaviour 
 {
 	enum State{
-		Wait,
+		Title,
+		Starting,
 		Ingame,
+		Quitting,
 	}
 	private State state; 
 
 	private long lastTimeStamp;
 
-	/// <summary>
-	/// ローカルのゲーム開始フラグ
-	/// </summary>
-	private byte startFlag = 0;
-
-	/// <summary>
-	/// ローカルのゲーム中断フラグ
-	/// </summary>
-	private byte quitFlag = 0;
-
 	void Awake()
 	{
-		startFlag = 0;
-		quitFlag = 0;
-
 		lastTimeStamp = -1;
 
-		UpdateState( State.Wait ); 
+		UpdateState( State.Title ); 
 
 		OSCHandler.Instance.InitServer();
 	}
 
+	float timer = 0;
+
 	// Update is called once per frame
 	void Update () 
 	{
-		string checkAddress = state == State.Wait ? "start" : "quit";
-		var stateAfter = state == State.Wait ? State.Ingame : State.Wait;
+		if( state == State.Starting || state == State.Quitting )
+		{
+				
+			// 遷移中はタイマーを更新
+			timer -= Time.deltaTime;
+			if( timer <= 0 )
+			{
+				var newState = state == State.Starting ? State.Ingame : State.Title;
+				UpdateState(newState);
+			}
 
-		CheckReceive( checkAddress, () => UpdateState( stateAfter ) );
+			return;
+		}
+
+		CheckReceive( );
 	}
 		
 	/// <summary>
 	/// 受信していないかチェック
 	/// </summary>
-	private void CheckReceive( string chkAddress, System.Action action )
+	private void CheckReceive()
 	{
+		// チェックするアドレスを指定する
+		string checkAddress = state == State.Title ? "start" : "quit";
+
+		// 受信を確認したあとのステートを指定する タイトル画面は開始中 ゲーム中は終了中
+		var stateAfter = state == State.Title ? State.Starting : State.Quitting;
+
 		//  受信データの更新
 		OSCHandler.Instance.UpdateLogs();
 		//  受信データの解析
@@ -64,14 +72,14 @@ public class ReceiveTest : MonoBehaviour
 					string address = item.Value.packets[i].Address;
 
 					// 適切なアドレスが来ていたら0番目の値でフラグを更新
-					if( address.Contains(chkAddress) )
+					if( address.Contains(checkAddress) )
 					{
 						byte result;
 						if( byte.TryParse( item.Value.packets[i].Data[0].ToString(), out result ))
 						{
 							if( result == 1 )
 							{
-								action();
+								UpdateState( stateAfter );
 							}
 						}
 					}
@@ -94,15 +102,27 @@ public class ReceiveTest : MonoBehaviour
 		string newText;
 		Color newPanelColor;
 
-		if( newState == State.Ingame )
+		switch( newState )
 		{
-			newText = "ゲームプレイ中";
+		case State.Title:
+			newText = "タイトル画面 命令を受信したらゲームを開始します";
+			newPanelColor = Color.gray;
+			break;
+		case State.Starting:
+			newText = "ゲームを開始しています";
+			newPanelColor = Color.cyan;
+			timer = 3f;
+			break;
+		case State.Ingame:
+			newText = "ゲームプレイ中 中断命令を受信したらゲームを中断します";
 			newPanelColor = Color.blue;
-		}
-		else
-		{
-			newText = "開始待機中";
+			break;
+		case State.Quitting:
+			default:
+			newText = "ゲーム中断中";
 			newPanelColor = Color.yellow;
+			timer = 3f;
+			break;
 		}
 
 		panelBg.color = newPanelColor;
